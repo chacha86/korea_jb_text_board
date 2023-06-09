76510d91a903108d675f66d043fe24e9891d076e
chacha86 <taejincha0619@gmail.com>
2023-06-09 11:54:41 +0900

    요구사항3 게시물 목록 출력 - 2

--- myboard/Main.java
+++ myboard/Main.java
@@ -1,12 +1,13 @@
 package myboard;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        String title = "";
-        String body = "";
+        ArrayList<String> titles = new ArrayList<>();
+        ArrayList<String> bodies = new ArrayList<>();
 
         while (true) {
             System.out.printf("명령어 : ");
@@ -20,16 +21,26 @@ public class Main {
             } else if (cmd.equals("add")) {
 
                 System.out.printf("게시물 제목을 입력해주세요 : ");
-                title = scan.nextLine();
+                String title = scan.nextLine();
                 System.out.printf("게시물 내용을 입력해주세요 : ");
-                body = scan.nextLine();
+                String body = scan.nextLine();
+
+                titles.add(title);
+                bodies.add(body);
+
                 System.out.println("게시물이 등록되었습니다.");
 
             } else if (cmd.equals("list")) {
+                if(titles.size() == 0) {
+                    System.out.println("등록된 게시물이 없습니다.");
+                    continue;
+                }
                 System.out.println("====================");
-                System.out.printf("제목 : %s\n", title);
-                System.out.printf("내용 : %s\n", body);
-                System.out.println("====================");
+                for(int i = 0; i < titles.size(); i++) {
+                    System.out.printf("제목 : %s\n", titles.get(i));
+                    System.out.printf("내용 : %s\n", bodies.get(i));
+                    System.out.println("====================");
+                }
             }
         }
     }
